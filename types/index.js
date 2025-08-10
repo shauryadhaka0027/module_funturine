b7358b7553b860f8896a5af1f5c1b97c475e0444
Shaurya Dhaka <138813918+shauryadhaka0027@users.noreply.github.com>
2025-08-10 10:56:19 +0530

    s

--- types/index.js
+++
@@ -1,8 +0,0 @@
-import { Request } from 'express';
-import { Document, Types } from 'mongoose';
-
-// Base User Interface
-export interface IUser extends Document { _id } { username } {
-  productCode, string>;
-  warranty= any> {
-  message
